b74ccc4122f434ee973b30c16ac4256b6acd6aea
sudhiitg <c.sudhanshu@iitg.ac.in>
2024-08-24 15:07:12 +0530

    add redox toolkit

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app =express();
-app.use(express.json());
+app.use(express.json());  
 
 app.listen(3000,() =>{
     console.log('Server is running on port 3000');
